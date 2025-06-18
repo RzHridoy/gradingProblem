c9a00a7604288cad996cf38aa2f449e6746c35ea
RzHridoy <rz.hridoy003@gmail.com>
2025-06-19 01:38:36 +0200

    commit_1

---
+++ grading_Problem.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int mark1, mark2, mark3;
+    printf("Enter mark1: \n");
+    scanf("%d", &mark1);
+    printf("Enter mark2: \n");
+    scanf("%d", &mark2);
+    printf("Enter mark3: \n");
+    scanf("%d", &mark3);
+
+    printf("The marks are: %d, %d, %d \n", mark1, mark2, mark3);
+
+    if(mark1 < 40 || mark2 < 40 || mark3 < 40)
+    {
+        printf("Studen is failed due to less passing mark!!! \n");
+    }
+    else if((mark1 + mark2 + mark3) / 3 < 50)
+    {
+        printf("Studen is failed due to less passing percentage!!! \n");
+    }
+    else
+    {
+        printf("Student is passed!!! \n");
+    }
+}
