bbf28ef956bf3e2073fed5065e06fb972e27cd9d
RzHridoy <rz.hridoy003@gmail.com>
2025-06-20 01:19:56 +0200

    commit_2

--- tax_Calculation.c
+++ tax_Calculation.c
@@ -2,6 +2,29 @@
 
 int main()
 {
-    float income, tax = 0;
-    
+    float earnings, tax = 0;
+    printf("Enter earnings amount: \n");
+    scanf("%f", &earnings);
+
+    if(earnings <= 250000)
+    {
+        tax = 0;
+    }
+
+    else if(earnings > 250000 && earnings <= 500000)
+    {
+        tax = 0.05 * (earnings - 250000);
+    }
+
+    else if(earnings > 500000 && earnings <= 1000000)
+    {
+        tax = 0.05 * (500000 - 250000) + 0.2 * (earnings - 500000);
+    }
+
+    else
+    {
+        tax = 0.05 * (500000 - 250000) + 0.2 * (1000000 - 500000) + 0.3 * (earnings - 1000000);
+    }
+
+    printf("The total amount of tax to pay is: %.2f", tax);
 }
