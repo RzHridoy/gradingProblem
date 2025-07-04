a1da5447ddb139e775ead347a4d886030e352cc7
RzHridoy <rz.hridoy003@gmail.com>
2025-07-04 10:37:57 +0200

    commit_1

--- factorial_findings.c
+++ factorial_findings.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
-int main()
+/*int main()
 {
     int initialize = 1;
-    int n = 5;
+    int n = 0;
 
     for (int i = initialize; i <= n; i++)
     {
@@ -11,5 +11,20 @@ int main()
     }
     printf("The factorial is: %d \n", initialize);
 
+    return 0;
+}*/
+
+
+int main()
+{
+    int initialize = 1, n = 5;
+    int i = initialize;
+    while (i <= n)
+    {
+        initialize *= i;
+        i++;
+    }
+    printf("The factorial of %d is: %d \n", n, initialize);
+    
     return 0;
 }
